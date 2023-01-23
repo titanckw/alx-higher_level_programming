c55330b117038969ed01749956631f93f5ca4e12
titanckw <karikuclemo@gmail.com>
2023-01-23 15:55:52 +0000

    Safe-Print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     c = 0
-    try:
-        for i in range(x):
-            print(my_list[i], end="")
+    for i in range(x):
+        try:
+            print(f"{my_list[i]}", end="")
             c += 1
-    except:
-        pass
+    except IndexError:
+        break
     print()
     return c
